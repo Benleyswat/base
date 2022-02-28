3acf3c03ccf9cf7093acd76aeb417d68268f3fc5
benleyswat <benleyswat@gmail.com>
2022-02-28 16:44:22 +0100

    new JUnit test and fixed error

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,6 +3,9 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
@@ -10,6 +13,12 @@ public class TrainSensorTest {
     @Before
     public void before() {
         // TODO Add initializations
+    }
+
+    @Test
+    public void NewAddExampleTest(){
+        Assert.assertEquals(25, 12 + 13);
+        
     }
 
     @Test
